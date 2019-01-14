6c35a892cc22c6c8b7e141039bf4b057a7443362
ShemelinPavel <pauls0@mail.ru>
2019-01-14 19:21:52 +0300

    ДЗ №1

---
+++ Task_10/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+
+Shemelin Pavel
+
+10
+
+Дано целое число N(> 0).
+С помощью операций деления нацело и взятия остатка от деления определить, имеются ли в записи числа N нечетные цифры.
+Если имеются, то вывести True, если нет — вывести False.
+
+*/
+
+using System;
+
+namespace Task_10
+{
+    class Program
+    {
+        /// <summary>
+        /// проверка числа на сожержание в нем нечетных цифр
+        /// </summary>
+        /// <param name="num">число для проверки</param>
+        /// <returns>результат - есть нечетные цифры (true/false)</returns>
+        public static bool IsNotEvenNumber( uint num )
+        {
+            //очередная цифра из числа - остаток деления на 10
+            uint divisionResidue_10 = 0;
+
+            while (num > 0)
+            {
+                divisionResidue_10 = num % 10;
+                num = num / 10;
+                if (divisionResidue_10 % 2 != 0) return true;
+            }
+            return false;
+        }
+
+        static void Main( string[] args )
+        {
+            uint Num1 = 75678;
+            uint Num2 = 426608;
+            bool res;
+
+            Console.WriteLine( $"Проверка числа {Num1}" );
+            res = IsNotEvenNumber( Num1 );
+            Console.WriteLine( res );
+
+            Console.WriteLine( $"Проверка числа {Num2}" );
+            res = IsNotEvenNumber( Num2 );
+            Console.WriteLine( res );
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_14/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+
+Shemelin Pavel
+
+14
+
+Автоморфные числа. Натуральное число называется автоморфным, если оно равно последним цифрам своего квадрата. Например, 25^2 = 625.
+Напишите программу, которая вводит натуральное число N и выводит на экран все автоморфные числа, не превосходящие N.
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14
+{
+    class Program
+    {
+        /// <summary>
+        /// печать автомарфных чисел 
+        /// </summary>
+        /// <param name="num">максимальное значение ряда натуральных чисел</param>
+        public static void PrintAutomorphNumbers( uint num )
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                //счетчик порядка натурального числа
+                uint decCounter = 1;
+
+                int currentNum = i / 10;
+                while (currentNum != 0)
+                {
+                    decCounter++;
+                    currentNum = currentNum / 10;
+                }
+
+                UInt32 res = (UInt32)Math.Pow( i, 2 );
+
+                if (res % ( Math.Pow( 10, decCounter ) ) == i) Console.WriteLine( i );
+            }
+        }
+
+        static void Main( string[] args )
+        {
+
+            Console.WriteLine( "Введите натуральное число:" );
+            bool res = uint.TryParse( Console.ReadLine(), out uint userAnswer );
+            if (res && userAnswer > 0) PrintAutomorphNumbers( userAnswer );
+            else Console.WriteLine( "Введеное значение не является натуральным числом!" );
+
+            Console.ReadKey();
+
+        }
+    }
+}
